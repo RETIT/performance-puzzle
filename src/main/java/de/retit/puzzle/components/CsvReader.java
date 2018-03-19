97e7100b947b9c6cdd407199616d7c25a5903f93
Evgeny Liskovets <eugene.liskovets@gmail.com>
2018-03-19 20:35:08 +0100

    Optimize reader

--- src/main/java/de/retit/puzzle/components/CsvReader.java
+++ src/main/java/de/retit/puzzle/components/CsvReader.java
@@ -1,5 +1,6 @@
 package de.retit.puzzle.components;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,36 +11,30 @@ import java.util.logging.Logger;
 
 public class CsvReader {
 
-	private static final Logger LOGGER = Logger.getLogger(CsvReader.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(CsvReader.class.getName());
 
-	private String fileLocation;
+    private String fileLocation;
 
-	public CsvReader(String fileLocation) {
-		this.fileLocation = fileLocation;
-	}
+    public CsvReader(String fileLocation) {
+        this.fileLocation = fileLocation;
+    }
 
-	public List<String> read() {
-		File file = new File(fileLocation);
-		if (!file.exists()) {
-			throw new IllegalStateException("File does not exist!");
-		}
-		List<String> lines = new ArrayList<>();
+    public List<String> read() {
+        File file = new File(fileLocation);
+        if (!file.exists()) {
+            throw new IllegalStateException("File does not exist!");
+        }
+        List<String> lines = new ArrayList<>();
+        String         line = "";
 		try (FileReader fileReader = new FileReader(file)) {
-			String line = "";
-			int character;
-			while ((character = fileReader.read()) > 0) {
-				if ((character == '\r' || character == '\n')) {
-					if (!line.equals("")) {
-						lines.add(line);
-						line = "";
-					}
-				} else {
-					line += (char) character;
-				}
-			}
+		    try(BufferedReader reader = new BufferedReader(fileReader)){
+                while((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
+            }
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, "Error reading input file file", e);
 		}
-		return lines;
-	}
+        return lines;
+    }
 }
