fce864aada93096ec813d2acaa9427b5f85b7504
Achim Rohn <achim@rohn.eu>
2018-03-19 19:56:32 +0100

    Write out the measurements line by line

--- src/main/java/de/retit/puzzle/components/ResultWriter.java
+++ src/main/java/de/retit/puzzle/components/ResultWriter.java
@@ -1,6 +1,7 @@
 package de.retit.puzzle.components;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,24 +28,33 @@ public class ResultWriter {
 
 	public void write() {
 		for (Entry<String, List<Measurement>> entry : transactionMap.entrySet()) {
-			String content = "";
-			for (int i = 0; i < entry.getValue().size(); i++) {
-				Measurement measurement = entry.getValue().get(i);
-				content += measurement.getTime().getTime();
-				content += ",";
-				content += measurement.getValue().longValue();
-				if (i < entry.getValue().size() - 1) {
-					content += "\n";
-				}
-			}
 			Path outputDirectoryPath = new File(outputDirectory).toPath();
 			Path outputFile = outputDirectoryPath.resolve(entry.getKey() + ".csv");
+			FileWriter writer;
 			try {
+				writer = new FileWriter(outputFile.toFile());
+				for (int i = 0; i < entry.getValue().size(); i++) {
+					Measurement measurement = entry.getValue().get(i);
+					String content = "";
+					content += measurement.getTime().getTime();
+					content += ",";
+					content += measurement.getValue().longValue();
+					if (i < entry.getValue().size() - 1) {
+						content += "\n";
+					}
+					writer.write(content);
+				}
+				writer.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			/* try {
 				Files.write(outputFile, content.getBytes(), StandardOpenOption.CREATE,
 						StandardOpenOption.TRUNCATE_EXISTING);
 			} catch (IOException e) {
 				LOGGER.log(Level.SEVERE, "Error writing CSV file for " + entry.getKey(), e);
-			}
+			}*/
 		}
 	}
 
