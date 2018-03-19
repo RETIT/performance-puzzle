bf88d05809b8e3109dc2cdc07c2f64c21c50180a
peter <peter@jetbrains.com>
2018-03-19 20:34:01 +0100

    less Future allocations

--- src/main/java/de/retit/puzzle/Puzzle.java
+++ src/main/java/de/retit/puzzle/Puzzle.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Puzzle {
 
@@ -39,32 +40,55 @@ public class Puzzle {
 		Map<String, BufferedWriter> outputs = new ConcurrentHashMap<>();
 
 		List<Future<?>> futures = Collections.synchronizedList(new ArrayList<>());
+		
+		LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
-		// Read input CSV data
-		new CsvReader(inputFile).read(line -> futures.add(executor.submit(() -> {
-			int comma1 = line.indexOf(',');
-			int comma2 = line.indexOf(',', comma1 + 1);
-			if (comma1 > 0 && comma2 > 0) {
-				String nums = line.substring(0, comma2);
-				String file = line.substring(comma2 + 1);
-				BufferedWriter writer = outputs.computeIfAbsent(file, s -> {
+		AtomicBoolean finished = new AtomicBoolean();
+
+		for (int i = 0; i < THREAD_COUNT; i++) {
+			futures.add(executor.submit(() -> {
+				while (true) {
+					String line;
 					try {
-						return new BufferedWriter(new FileWriter(new File(outputDirectory, s + ".csv")));
-					} catch (IOException e) {
+						line = queue.poll(10, TimeUnit.MILLISECONDS);
+					} catch (InterruptedException e) {
 						throw new RuntimeException(e);
 					}
-				});
-				//noinspection SynchronizationOnLocalVariableOrMethodParameter
-				synchronized (writer) {
-					try {
-						writer.write(nums);
-						writer.write('\n');
-					} catch (IOException e) {
-						e.printStackTrace();
+					if (line == null) {
+						if (finished.get()) break;
+						continue;
 					}
+					
+					int comma1 = line.indexOf(',');
+					int comma2 = line.indexOf(',', comma1 + 1);
+					if (comma1 > 0 && comma2 > 0) {
+						String nums = line.substring(0, comma2);
+						String file = line.substring(comma2 + 1);
+						BufferedWriter writer = outputs.computeIfAbsent(file, s -> {
+							try {
+								return new BufferedWriter(new FileWriter(new File(outputDirectory, s + ".csv")));
+							} catch (IOException e) {
+								throw new RuntimeException(e);
+							}
+						});
+						//noinspection SynchronizationOnLocalVariableOrMethodParameter
+						synchronized (writer) {
+							try {
+								writer.write(nums);
+								writer.write('\n');
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+					
 				}
-			}
-		})));
+			}));
+		}
+
+		// Read input CSV data
+		new CsvReader(inputFile).read(queue::add);
+		finished.set(true);
 
 		for (Future<?> future : futures) {
 			future.get();
